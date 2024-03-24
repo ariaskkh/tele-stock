d7270572ca98fdac80c2b1c859d10c5f9ad18a30
KANGHO <ariaskkh@gmail.com>
2024-03-24 18:25:04 +0900

    (연습) 취득 예정 주식 수 및 주가 확인

--- src/example/treasury_stock.py
+++ src/example/treasury_stock.py
@@ -2,7 +2,10 @@ import sys
 sys.path.append('/Users/dean/Desktop/programming/tele-stock/src')
 import requests
 import pandas as pd
+import FinanceDataReader as fdr
+import matplotlib.pyplot as plt
 from private_data import DART_API_KEY
+
 
 
 ## 자기주식취득 공시 데이터 가져오기
@@ -103,7 +106,65 @@ details=details[['rcept_no','aqpln_stk_ostk','aqpln_stk_estk',
 
 details=details.set_index('rcept_no')
 
-print(details)
+# print(details)
 
 
 ###############################################################
+
+# 취득 예정 주식 수 및 주가 확인
+
+# axis = 1은 데이터 좌우로 병합, join = 'inner'은 교집합 병합 <-> 'outer'는 합집합 병합
+df_all = pd.concat([df, details], axis = 1, join='inner')
+
+cols=['aqpln_stk_ostk','aqpln_stk_estk',
+     'aqpln_prc_ostk','aqpln_prc_estk','aq_wtn_div_ostk','aq_wtn_div_estk','eaq_ostk','eaq_estk']
+
+#object -> int 변환
+for col in cols:
+    df_all[col] = df_all[col].str.replace(',', '').replace('-','0').replace('','0')
+
+df_all[cols]=df_all[cols].astype('int64')
+df_all['rcept_date'] = pd.to_datetime(df_all['rcept_dt'], format='%Y%m%d') # 20220115 -> 2022-01-15
+df_all['aqexpd_bgd'] = pd.to_datetime(df_all['aqexpd_bgd'], format='%Y년 %m월 %d일')
+df_all['aqexpd_edd'] = pd.to_datetime(df_all['aqexpd_edd'], format='%Y년 %m월 %d일')
+
+
+colsForResult=['corp_name','stock_code','rcept_dt','rcept_date','aqpln_stk_ostk','aqpln_stk_estk',
+     'aqpln_prc_ostk','aqpln_prc_estk', 'aqexpd_bgd']
+for index, r in df_all[colsForResult].iterrows():
+    corp_name = r['corp_name']
+    stock_code = r['stock_code']
+    rcept_date = r['rcept_date'] # 공시 접수 일자
+
+    aqpln_stk_ostk = r['aqpln_stk_ostk'] # 취득예정주식(주)(보통주식)
+    aqpln_stk_estk = r['aqpln_stk_estk'] # 취득예정주식(주)(기타주식)
+    aqpln_prc_ostk = r['aqpln_prc_ostk'] # 취득예정금액(원)(보통주식)
+    aqpln_prc_estk = r['aqpln_prc_estk'] # 취득예정금액(원)(기타주식)
+    aqexpd_bgd = r['aqexpd_bgd'] # 취득 예상 기간(시작일)
+    
+    print('corp_name: ' + corp_name)
+    print('접수일자 : ' + rcept_date.strftime('%Y-%m-%d')) # strftime: obt(Timestamp) -> str
+    print('취득예정주식(주)보통주식 : ' + str(aqpln_stk_ostk))
+    print('취득예정주식(주)보통주식 : ' + str(aqpln_stk_ostk))
+    print('취득예정금액(원)(보통주식) : ' + str(aqpln_prc_ostk))
+    print('취득예정금액(원)(기타주식) : ' + str(aqpln_prc_estk))
+    print('취득예상기간(시작일) : ' + aqexpd_bgd.strftime('%Y-%m-%d')) 
+
+
+    # 주가정보
+    code = stock_code
+    startdate = '2023-12-01'
+    enddate = '2024-03-24'
+    # Close(종가), Open, High, Low, Volume, Change
+    df_stock = fdr.DataReader(code, startdate, enddate)[['Close']]
+
+    plt.rc('font', family='AppleGothic')
+    plt.figure(figsize=(6,2))
+    plt.plot(df_stock.index, df_stock['Close']) # x, y축
+    plt.title(corp_name)
+    plt.xticks(rotation = 90) # x축 라벨
+    
+    plt.axvline(x = rcept_date, color = 'red') #공시 접수일자
+    if aqexpd_bgd in df_stock.index:
+        plt.axvline(x = aqexpd_bgd, color = 'green') #취득예상기간(시작일)
+    plt.show()
