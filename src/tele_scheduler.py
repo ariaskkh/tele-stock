8fc1096d1efb71e9cd2fa9215a6c3280dc45f19e
KANGHO <ariaskkh@gmail.com>
2024-05-05 17:40:16 +0900

    tele_scheduler에서 signal이 비동기 처리하도록 수정

--- src/tele_scheduler.py
+++ src/tele_scheduler.py
@@ -1,8 +1,9 @@
 import signal
 import time
 from datetime import datetime, time, timedelta
+import asyncio
 
-REPEAT_TIME_FOR_CHECK = 60 * 2
+REPEAT_TIME_FOR_CHECK = 2
 START_HOUR = 9
 START_MINUTE = 0
 END_HOUR = 15
@@ -10,8 +11,9 @@ END_MINUTE = 30
 
 class TeleScheduler:
     __repeat_time_for_check = REPEAT_TIME_FOR_CHECK
-    def __init__(self, func, message = "") -> None:
-        handler = self.__alarm_handler_factory(func, message)
+
+    def __init__(self, loop, func, message = "") -> None:
+        handler = self.__alarm_handler_factory(loop, func, message)
         # set up signal handler
         signal.signal(signal.SIGALRM, handler)
     
@@ -29,12 +31,12 @@ class TeleScheduler:
         time_left_for_next_start: timedelta = next_start_time - now
         return time_left_for_next_start.total_seconds()
 
-    def __alarm_handler_factory(self, func, message):
+    def __alarm_handler_factory(self, loop, func, params):
         # signum: identifier
         # frame: current stack frame. execution state를 나타내는 object
         def __alarm_handler(signum, frame) -> None:
             if self.__is_working_hour():
-                func(message)
+                asyncio.run_coroutine_threadsafe(func(params), loop)
                 signal.alarm(self.__repeat_time_for_check)
             else:
                 self.__wait_until_start()
@@ -49,26 +51,5 @@ class TeleScheduler:
             self.__wait_until_start()
 
     def __wait_until_start(self) -> None:
-        time.sleep(self.__get_time_left_for_start())
+        asyncio.sleep(self.__get_time_left_for_start())
         self.start_alarm()
-            
-
-
-## 사용 예
-
-# def main():
-#     scheduler_signal = TeleScheduler()
-#     print("scheduler 시작")
-#     if scheduler_signal.is_working_hour():
-#         scheduler_signal.start_alarm()
-#     else:
-#         scheduler_signal.wait_until_start()
-
-#     try:
-#         while True:
-#             time.sleep(1) # infinite loop for Program running
-#     except KeyboardInterrupt:
-#         print("프로그램 종료")
-
-# if __name__ == "__main__":
-#     main()
--- src/telebot.py
+++ src/telebot.py
@@ -1,37 +1,43 @@
 from private_data import TOKEN_ID, CHAT_ID
 import telegram
+from tele_scheduler import TeleScheduler
 from treasury_stock_data import TreasuryStock
 import asyncio
 
 class TeleBot:
     treasury_stock: TreasuryStock
-    def __init__(self) -> None:
-        self.bot = telegram.Bot(token=TOKEN_ID)
-
-    def add_treasury_stock(self) -> None:
-        self.treasury_stock = TreasuryStock()
+    scheduler: TeleScheduler
     
-    async def send_stock_tele_message(self) -> None:
-        if self.treasury_stock:
-            treasury_stock_tele_message = self.__get_stock_tele_message()
-            if treasury_stock_tele_message != None:
-                for message in treasury_stock_tele_message:
-                    await self.bot.send_message(chat_id = CHAT_ID, text = message)
-        else:
-            print("Treasury_stock 인스턴스가 생성되지 않았습니다.")
+    def __init__(self, loop) -> None:
+        self.bot = telegram.Bot(token=TOKEN_ID)
+        self.scheduler = TeleScheduler(loop, self.__send_message)
+
+    def start_sending_message(self) -> None:
+        self.scheduler.start_alarm()
+
+    async def __send_message(self, params = None) -> None:
+        self.treasury_stock = TreasuryStock()
+        treasury_stock_tele_message = self.__get_stock_tele_message()
+        if treasury_stock_tele_message != None:
+            for message in treasury_stock_tele_message:
+                await self.bot.send_message(chat_id = CHAT_ID, text = message)
 
     def __get_stock_tele_message(self) -> list:
         return self.treasury_stock.get_stock_tele_messages()
-    
 
-async def main():
+def main():
     print("==== 프로그램 시작 !! ====")
 
-    telebot = TeleBot()
-    telebot.add_treasury_stock()
-    await telebot.send_stock_tele_message()
+    loop = asyncio.get_event_loop()
+    telebot = TeleBot(loop)
+    telebot.start_sending_message()
 
-    print("==== 프로그램 종료 !! ====")
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print("==== 프로그램 종료 !! ====")
+    finally:
+        loop.close()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
